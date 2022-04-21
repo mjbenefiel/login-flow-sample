4eb5867475ef2b56524d52c47eeeeb33fea34d44
mjbenefiel <michaeljbenefiel@gmail.com>
2022-04-21 09:26:37 -0700

    loading view controller update

---
+++ login-flow-sample/controller/LoadingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingViewController.swift
+//  login-flow-sample
+//
+//  Created by Michael Benefiel on 4/21/22.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .orange
+    }
+}
