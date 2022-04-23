eba6f797f13f40db33461c00836286eff7f0ef60
mjbenefiel <michaeljbenefiel@gmail.com>
2022-04-22 23:27:24 -0700

    Onboarding screen functionality

--- login-flow-sample/controller/OnboardingViewController.swift
+++ login-flow-sample/controller/OnboardingViewController.swift
@@ -9,13 +9,26 @@ import Foundation
 import UIKit
 
 class OnboardingViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       // setupViews()
+       setupViews()
+       setupCollectionView()
     }
-//    private func setupViews(){
-//        view.backgroundColor = .red
-//    }
+    
+    private func setupCollectionView(){
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        collectionView.backgroundColor = .systemGroupedBackground
+        collectionView.collectionViewLayout = layout
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+    }
+    private func setupViews(){
+     
+    }
     
     @IBAction func getStartedButtonTapped(_sender: UIButton){
        // print("get started button tapped")
@@ -23,3 +36,24 @@ class OnboardingViewController: UIViewController {
         
     }
 }
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "containerCell", for: indexPath)
+        cell.backgroundColor = indexPath.row % 2 == 0 ? .blue : .red
+            return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+
