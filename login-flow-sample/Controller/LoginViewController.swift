2a3328aa9aeb1133fabef3ea7fd67018dbf2717a
mjbenefiel <michaeljbenefiel@gmail.com>
2022-04-25 10:01:33 -0700

    UI login screen - stack view

---
+++ login-flow-sample/Controller/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  login-flow-sample
+//
+//  Created by Michael Benefiel on 4/25/22.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
