4f3ed85b1e61bf01b9db576e3280e852dea1dfd9
mjbenefiel <michaeljbenefiel@gmail.com>
2022-04-26 14:58:42 -0700

    handle logout user

--- login-flow-sample/Controller/SettingsViewController1.swift
+++ login-flow-sample/Controller/SettingsViewController1.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 import MBProgressHUD
+import FirebaseAuth
 
 class SettingsViewController1: UIViewController{
     override func viewDidLoad() {
@@ -21,10 +22,28 @@ class SettingsViewController1: UIViewController{
     
     
     @IBAction func logoutButtonTapped(_ sender: UIBarButtonItem){
+        
+//        let firebaseAuth = Auth.auth()
+//        do {
+//            try firebaseAuth.signOut()
+//        } catch let signOutError as NSError {
+//            print("Error signing out: %@", signOutError)
+//        }
+        
+
+        
         MBProgressHUD.showAdded(to: view, animated: true)
         delay(durationInSeconds: 0.5) {
-            MBProgressHUD.hide(for: self.view, animated: true)
-            PresenterManager.shared.show(vc: .onboarding )
+            
+            do {
+                try Auth.auth().signOut()
+                MBProgressHUD.hide(for: self.view, animated: true)
+                PresenterManager.shared.show(vc: .onboarding )
+            } catch(let error) {
+                print(error.localizedDescription)
+                MBProgressHUD.hide(for: self.view, animated: true)
+            }
+            
         }
         
     }
