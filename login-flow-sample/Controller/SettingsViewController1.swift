4f7f9d43e8e642ca3a6ad1d24dddb00f201ac154
mjbenefiel <michaeljbenefiel@gmail.com>
2022-04-27 14:22:56 -0700

    Settings page initial

--- login-flow-sample/Controller/SettingsViewController1.swift
+++ login-flow-sample/Controller/SettingsViewController1.swift
@@ -10,15 +10,55 @@ import UIKit
 import MBProgressHUD
 import Loaf
 
+
+private let reuseIdentifier = "SettingsCell"
+
 class SettingsViewController1: UIViewController{
     
     private let authManager = AuthManager()
+
+    
+    // MARK: - Properties
+    
+    var tableView: UITableView!
+    var userInfoHeader: UserInfoHeader!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+        configureUI()
         
     }
+    
+    // MARK: - Helper Functions
+    
+    func configureTableView() {
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 60
+        
+        tableView.register(SettingsCell.self, forCellReuseIdentifier: reuseIdentifier)
+        view.addSubview(tableView)
+        tableView.frame = view.frame
+        
+        let frame = CGRect(x: 0, y: 88, width: view.frame.width, height: 100)
+        userInfoHeader = UserInfoHeader(frame: frame)
+        tableView.tableHeaderView = userInfoHeader
+        tableView.tableFooterView = UIView()
+    }
+    
+    func configureUI() {
+        configureTableView()
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.barTintColor = UIColor(red: 55/255, green: 120/255, blue: 250/255, alpha: 1)
+        navigationItem.title = "Settings"
+    }
+    
+    
     
     
     
@@ -44,4 +84,22 @@ class SettingsViewController1: UIViewController{
         }
         
     }
+    
+    
+    
+    
+}
+
+extension SettingsViewController1: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! SettingsCell
+        return cell
+    }
+    
+    
 }
